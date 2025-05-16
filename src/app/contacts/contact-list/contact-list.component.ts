427a5e29140d61609b85d9e96eed123f9aaabeb1
Calebjorgensen <121927129+Calebjorgensen@users.noreply.github.com>
2025-05-16 15:16:31 -0700

    inital commit

---
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'app-contact-list',
+  standalone: false,
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.css'
+})
+export class ContactListComponent {
+
+    contacts: Contact[] = [
+      new Contact(1, 'R. Kent Jackson', 'jacksonk@byui.edu', 2084963771, '../../assets/images/jacksonk.jpg', null),
+      new Contact(2, 'Rex Barzee', 'barzeer@byui.edu', 2084963768, '', null)
+    ];
+}
