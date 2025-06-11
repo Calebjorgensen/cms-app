dc020f818492702d7863fc0011496fe2adc8661e
Timothy Harris <harristi@byui.edu>
2025-06-10 21:45:15 -0400

    fix: resolved issue with contact images and fixed message send

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -11,20 +11,15 @@ import { MessageService } from '../message.service';
 export class MessageEditComponent {
     currentSender = 'Caleb Jorgensen'
 
-    @ViewChild('id') idInput: ElementRef;
     @ViewChild('subject') subjectInput: ElementRef;
     @ViewChild('msgText') msgTextInput: ElementRef;
-    @ViewChild('sender') senderInput: ElementRef;
-
 
     constructor(private messageService: MessageService) {}
 
   onSendMessage() {
-    const mId = this.idInput.nativeElement.value;
     const mSubject = this.subjectInput.nativeElement.value;
     const mMsgText = this.msgTextInput.nativeElement.value;
-    const mSender = this.senderInput.nativeElement.value;
-    const newMessage = new Message(mId, mSubject, mMsgText, mSender);
+    const newMessage = new Message('', mSubject, mMsgText, '8'); // We need a default sender with an Id that exsists in contacts or a way to handle unknown senders.
     this.messageService.addMessage(newMessage);
   }
 
